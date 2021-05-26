67ef064aa78caff2389952a491a21c4f37ab51b6
doniataskman <doniahammud@gmail.com>
2021-05-26 11:30:55 +0300

    New Project

---
+++ app/src/main/java/aliawad/finalprojectali/Data/Thing.java
@@ -0,0 +1,15 @@
+package aliawad.finalprojectali.Data;
+
+public class Thing {
+    public String Title;
+    public String Category;
+    public String Link;
+    public String Image;
+}
+public Title title;
+public Categoty categoty;
+public Link link;
+public Image image;
+
+
+
