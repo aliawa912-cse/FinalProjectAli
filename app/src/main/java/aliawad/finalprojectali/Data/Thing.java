855c56a0a4e47dc767d4184a2c557aa1ec9a44a0
doniataskman <doniahammud@gmail.com>
2021-06-08 11:36:30 +0300

    New Thing

--- app/src/main/java/aliawad/finalprojectali/Data/Thing.java
+++ app/src/main/java/aliawad/finalprojectali/Data/Thing.java
@@ -1,15 +1,81 @@
 package aliawad.finalprojectali.Data;
 
-public class Thing {
+public class Thing{
     public String Title;
     public String Category;
     public String Link;
     public String Image;
+    private String key;
+    private String owner;
+
+    public static boolean isSuccessful() {
+        return successful;
+    }
+
+    public static void setSuccessful(boolean successful) {
+        Thing.successful = successful;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public String getCategory() {
+        return Category;
+    }
+
+    public void setCategory(String category) {
+        Category = category;
+    }
+
+    public String getLink() {
+        return Link;
+    }
+
+    public void setLink(String link) {
+        Link = link;
+    }
+
+    public String getImage() {
+        return Image;
+    }
+
+    public void setImage(String image) {
+        Image = image;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    @Override
+    public String toString() {
+        return "Thing{" +
+                "Title='" + Title + '\'' +
+                ", Category='" + Category + '\'' +
+                ", Link='" + Link + '\'' +
+                ", Image='" + Image + '\'' +
+                ", key='" + key + '\'' +
+                ", owner='" + owner + '\'' +
+                '}';
+    }
 }
-public Title title;
-public Categoty categoty;
-public Link link;
-public Image image;
 
 
 
